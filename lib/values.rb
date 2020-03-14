6125c2f25c129f8ed3705bb476cee7805c50ac0b
Jose E Reyes <jreyes8631@gmail.com>
2020-03-14 21:57:57 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -17,3 +17,24 @@ def get_the_values(groceries)
 }
 
 end
+
+
+def get_the_keys(groceries)
+  groceries.keys
+end
+
+get_the_keys(groceries)
+
+
+def get_the_values(groceries)
+  #code your solution here!
+  groceries.values 
+  groceries.values.flatten 
+end
+
+get_the_values(groceries)
+
+
+def get_the_min(groceries)
+  groceries.values.flatten.min
+end
